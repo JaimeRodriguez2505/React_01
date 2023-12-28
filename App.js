ccae3bfc35d797817270be151cfbef2b3ac93325
JaimeRodriguez2505 <112582195+JaimeRodriguez2505@users.noreply.github.com>
2023-12-28 01:56:25 -0500

    t

---
+++ App.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { View, Image, TextInput, TouchableOpacity, Text, StyleSheet } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Image source={require('./assets/logo.png')} style={styles.logo} />
+      <TextInput
+        style={styles.input}
+        placeholder="Usuario"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Contraseña"
+        secureTextEntry={true}
+      />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Ingresar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    marginBottom: 20,
+  },
+  input: {
+    width: 300,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  },
+  button: {
+    backgroundColor: 'blue',
+    padding: 10,
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 16,
+  },
+});
+
+export default App;
